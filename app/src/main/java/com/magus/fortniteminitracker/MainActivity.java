f07ab9abe1056831f0880d6095b79fa48f9e425e
mggoro <martingoro@gmail.com>
2020-03-08 19:58:59 -0300

    initial commit

--- app/src/main/java/com/magus/fortniteminitracker/MainActivity.java
+++ app/src/main/java/com/magus/fortniteminitracker/MainActivity.java
@@ -2,13 +2,28 @@ package com.magus.fortniteminitracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.magus.fortniteminitracker.UserPSNFragment.OnFragmentInteractionListener;
+
+public class MainActivity extends AppCompatActivity implements OnFragmentInteractionListener {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
--- app/src/main/java/com/magus/fortniteminitracker/ui/userquery/UserQueryViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.magus.fortniteminitracker.ui.userquery;
-
-import androidx.lifecycle.ViewModel;
-
-public class UserQueryViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
