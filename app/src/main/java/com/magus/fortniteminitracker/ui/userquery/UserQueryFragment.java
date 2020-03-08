8661ef340cd7441e77218daa37a3839b21c60a01
mggoro <martingoro@gmail.com>
2020-03-08 17:50:02 -0300

    initial commit

---
+++ app/src/main/java/com/magus/fortniteminitracker/ui/userquery/UserQueryFragment.java
@@ -0,0 +1,39 @@
+package com.magus.fortniteminitracker.ui.userquery;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.magus.fortniteminitracker.R;
+
+public class UserQueryFragment extends Fragment {
+
+    private UserQueryViewModel mViewModel;
+
+    public static UserQueryFragment newInstance() {
+        return new UserQueryFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.user_query_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(UserQueryViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/magus/fortniteminitracker/ui/userquery/UserQueryViewModel.java
@@ -0,0 +1,7 @@
+package com.magus.fortniteminitracker.ui.userquery;
+
+import androidx.lifecycle.ViewModel;
+
+public class UserQueryViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
