b76623bb050ffd21ff2e62a9d67a197ca0bb60a4
mggoro <martingoro@gmail.com>
2020-03-13 00:20:28 -0300

    consulta el servicio de fortnite tracker y muestra los datos
    falta ordenar el frafment, corregido
    
    falta pasar como parametro el usuario de psn, corregido, hay que probarlo
    
    es multiplataforma al buscar usuario. hay que probarlo
    
    utiliza retrofit con varios paramettros en el url

--- app/src/main/java/com/magus/fortniteminitracker/Retrofit/MiniTrackerService.java
+++ app/src/main/java/com/magus/fortniteminitracker/Retrofit/MiniTrackerService.java
@@ -6,11 +6,20 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface MiniTrackerService {
 
     //profile/psn/cardenal_borgia
-     @GET("profile/psn/cardenal_borgia")
-     @Headers("TRN-Api-Key:b5507bd6-b32b-4012-b107-1fa1c6c56368")
-     Call<ResponseFindUser> doConsultaUsuario();
+    // @GET("profile/psn/cardenal_borgia")
+    // @Headers("TRN-Api-Key:b5507bd6-b32b-4012-b107-1fa1c6c56368")
+    // Call<ResponseFindUser> doConsultaUsuario();
+
+
+    //profile/psn/cardenal_borgia
+    @GET("profile/{plataforma}/{idUser}")
+    @Headers("TRN-Api-Key:b5507bd6-b32b-4012-b107-1fa1c6c56368")
+    Call<ResponseFindUser> doConsultaUsuario(@Path("idUser") String idUser,@Path("plataforma")String plataforma);
+
+
 }
--- app/src/main/java/com/magus/fortniteminitracker/UserPSNFragment.java
+++ app/src/main/java/com/magus/fortniteminitracker/UserPSNFragment.java
@@ -12,6 +12,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.magus.fortniteminitracker.Retrofit.MiniTrackerClient;
@@ -35,6 +37,9 @@ public class UserPSNFragment extends Fragment {
     private Button btnVerEstadisticas;
     MiniTrackerService miniTrackerService;
     MiniTrackerClient miniTrackerClient;
+    private TextView tvUsuarioPsn;
+    public String sUsuarioFortnite ;
+    public String sPlataforma = "PSN";
 
     private  View nView ;
 
@@ -53,54 +58,63 @@ public class UserPSNFragment extends Fragment {
 
         // Inflate the layout for this fragment
         nView = inflater.inflate(R.layout.fragment_user_psn, container, false);
+        tvUsuarioPsn = nView.findViewById(R.id.editTextUsuarioPsn);
+
          btnVerEstadisticas = (Button) nView.findViewById(R.id.buttonVerUsuario);
          btnVerEstadisticas.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(final View v) {
 
-                 // aca llamo al servicio y busco la info
+                 sUsuarioFortnite = tvUsuarioPsn.getText().toString();
+
+                 if (sUsuarioFortnite.length() > 0) {
+
+                     // aca llamo al servicio y busco la info
+                     Call<ResponseFindUser> call = miniTrackerService.doConsultaUsuario(sUsuarioFortnite,sPlataforma);
+                     call.enqueue(new Callback<ResponseFindUser>() {
+                         @Override
+                         public void onResponse(Call<ResponseFindUser> call, Response<ResponseFindUser> response) {
+                             // por aca todo bien
+                             if (response.isSuccessful()) {
 
 
-                 Call<ResponseFindUser> call = miniTrackerService.doConsultaUsuario();
-                 call.enqueue(new Callback<ResponseFindUser>() {
-                     @Override
-                     public void onResponse(Call<ResponseFindUser> call, Response<ResponseFindUser> response) {
-                        // por aca todo bien
-                         if (response.isSuccessful()) {
+                                 //todo way
+                                 // llamo a la siguiente ventana
+                                 if (response.body().getAccountId() != null) {
+                                     Intent itEstadisticas = new Intent(getActivity(), EstadisticasUsuarioActivity.class);
+
+                                     itEstadisticas.putExtra("account", response.body().getAccountId());
+                                     itEstadisticas.putExtra("epicUser", response.body().getEpicUserHandle());
+                                     itEstadisticas.putExtra("wins", response.body().getStats().getP2().getTop1().getValue());
+                                     itEstadisticas.putExtra("kills", response.body().getStats().getP2().getKills().getValue());
+                                     itEstadisticas.putExtra("winRatio", response.body().getStats().getP2().getWinRatio().getValue());
+                                     itEstadisticas.putExtra("matchs", response.body().getStats().getP2().getMatches().getValue());
+                                     itEstadisticas.putExtra("killsPorGame", response.body().getStats().getP2().getKpg().getValue());
+                                     itEstadisticas.putExtra("top10", response.body().getStats().getP2().getTop10().getValue());
+                                     itEstadisticas.putExtra("top25", response.body().getStats().getP2().getTop25().getValue());
 
 
-                             //todo way
-                             // llamo a la siguiente ventana
-                             if ( response.body().getAccountId() != null ) {
-                                 Intent itEstadisticas = new Intent(getActivity(), EstadisticasUsuarioActivity.class);
+                                     startActivity(itEstadisticas);
+                                 } else {
+                                     Toast.makeText(getActivity(),"No se encuentra el usuario en Epic", Toast.LENGTH_LONG);
+                                 }
+                                 //destruimos el activity
 
-                                 itEstadisticas.putExtra("account", response.body().getAccountId());
-                                 itEstadisticas.putExtra("epicUser", response.body().getEpicUserHandle());
-                                 itEstadisticas.putExtra("wins", response.body().getStats().getP2().getTop1().getValue());
-                                 itEstadisticas.putExtra("kills", response.body().getStats().getP2().getKills().getValue());
-                                 itEstadisticas.putExtra("winRatio", response.body().getStats().getP2().getWinRatio().getValue());
-                                 itEstadisticas.putExtra("matchs", response.body().getStats().getP2().getMatches().getValue());
-                                 itEstadisticas.putExtra("killsPorGame", response.body().getStats().getP2().getKpg().getValue());
-                                 itEstadisticas.putExtra("top10", response.body().getStats().getP2().getTop10().getValue());
-                                 itEstadisticas.putExtra("top25", response.body().getStats().getP2().getTop25().getValue());
-
-
-                                 startActivity(itEstadisticas);
                              }
-                             //destruimos el activity
+                         }
+
+                         @Override
+                         public void onFailure(Call<ResponseFindUser> call, Throwable t) {
+                             Log.i("CALL", "un pequeño error " + t.fillInStackTrace());
+                             Toast.makeText(getContext(), "hubo un errror al consutlar", Toast.LENGTH_LONG);
 
                          }
-                     }
-
-                     @Override
-                     public void onFailure(Call<ResponseFindUser> call, Throwable t) {
-                         Log.i("CALL","un pequeño error " + t.fillInStackTrace());
-                         Toast.makeText(getContext(), "hubo un errror al consutlar", Toast.LENGTH_LONG);
-
-                     }
-                 });
+                     });
 
 
+                 } else{
+                     tvUsuarioPsn.setError("Debe Completar un usuario de Fortnite");
+                 }
              }
          });
          return nView;
@@ -128,6 +142,28 @@ public class UserPSNFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radioButtonPSN:
+                if (checked)
+                    sPlataforma = "psn";
+                    break;
+            case R.id.radioButtonXbox:
+                if (checked)
+                    sPlataforma = "xbl";
+                    break;
+            case R.id.radioButtonPC:
+                if (checked)
+                    sPlataforma = "pc";
+                    break;
+
+        }
     }
 
     /**
